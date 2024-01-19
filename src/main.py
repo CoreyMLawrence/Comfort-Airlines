982efa8bfd6e8ad221c4905c31cfbdaf1256f3fc
Ethan Cox <ethanrcox@gmail.com>
2024-01-19 12:49:45 -0500

    abstacted out databse constants; standardized documentation names to nouns

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 import mariadb
 import sys
 
+DATABASE_USERNAME="root"
+DATABASE_PASSWORD="toor"
+DATABASE_IP="172.17.0.1"
+DATABASE_PORT=3306
+DATABASE_NAME="comfort-airlines-db"
+
+
 def main() -> None:
     """The entry point for the program"""
     print("Hello, Docker!")
     try:
         connection = mariadb.connect(
-            user="root",
-            password="toor",
-            host="172.17.0.1",
-            port=3306,
-            database="comfort-airlines-db"
+            user=DATABASE_USERNAME,
+            password=DATABASE_PASSWORD,
+            host=DATABASE_IP,
+            port=DATABASE_PORT,
+            database=DATABASE_NAME
         )
         
         cursor = connection.cursor()
