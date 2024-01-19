7b3c5fdbf00063d9c7806eaa68755f448db0fa43
Ethan Cox <ethanrcox@gmail.com>
2024-01-19 12:00:37 -0500

    added docker skeleton. configuration unfinished

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
+import mariadb
+import sys
+
 def main() -> None:
     """The entry point for the program"""
-    pass
+    print("Hello, Docker!")
+    try:
+        connection = mariadb.connect(
+            user="root",
+            password="toor",
+            host="192.0.2.1",
+            port=3306,
+            database="comfort-airlines-db"
+        )
+        
+        cursor = connection.cursor()
+        print("Sucessfully connected to the database")
+        
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
