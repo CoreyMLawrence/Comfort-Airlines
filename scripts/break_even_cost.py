ed51414d9a066fa042d9f88bc84b3a886077077a
PBlue2001 <148484584+PBlue2001@users.noreply.github.com>
2024-01-23 12:25:35 -0500

    Create break_even_cost.py
    started break even calculation, first save point, not working currently

---
+++ scripts/break_even_cost.py
@@ -0,0 +1,33 @@
+import csv
+from pprint import pprint
+
+# Indices of values in flight_demand.csv
+SOURCE_AIRPORT = 0
+DESTINATION_AIRPORT = 1
+DISTANCE_KM = 2
+
+#distance matrix
+
+# Formula constants
+takeoff_fee = 2000
+landing_fee = 2000
+gas = 6.19#gallon
+
+# File-reading constants
+FILE_START = 0
+
+
+with open ("data/flight_demand.csv", "r") as flight_demand, open("data/flights.csv", "r") as flight_data:
+    # 1. Generete a mapping of airports to their metro populations
+    reader = csv.reader(flight_demand, delimiter=',')
+    header = next(reader)
+    metro_population = dict(
+        (airport[NAME],int(airport[METRO_POPULATION])) for airport in reader
+    )
+
+
+
+#extracting distances from distance matrix.csv
+for i in range(31):
+    for j in range(i+1, 31):
+        #grab distance from csv
