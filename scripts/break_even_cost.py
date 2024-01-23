c91551c6a04ffc3ff4d684012c4eace88082f14b
PBlue2001 <148484584+PBlue2001@users.noreply.github.com>
2024-01-23 12:55:01 -0500

    Update break_even_cost.py

--- scripts/break_even_cost.py
+++ scripts/break_even_cost.py
@@ -7,27 +7,30 @@ DESTINATION_AIRPORT = 1
 DEMAND = 2
 DISTANCE_KM = 3
 
+#aircraftSpecs.csv, fucked for time
+Boeing_737_600 = .55 
+Boeing_767_800  = .44
+Airbus_A200_100 = .57
+Airbus_A200_300 = .66
+
 
 # Formula constants
 takeoff_fee = 2000
 landing_fee = 2000
 gas = 6.19#gallon
+KMtoM = 0.621371#converst to miles
 
 # File-reading constants
 FILE_START = 0
 
 
-with open ("data/flight_demand.csv", "r") as flight_demand, open("data/flights.csv", "r") as flight_data:
-    # 1. Generete a mapping of airports to their metro populations
-    reader = csv.reader(flight_demand, delimiter=',')
+with open ("data/flights.csv", "r") as flights:
+    #Generate break even costs
+    reader = csv.reader(flights, delimiter=',')
     header = next(reader)
-    metro_population = dict(
-        (airport[NAME],int(airport[METRO_POPULATION])) for airport in reader
-    )
+    for row in flights:
+        print(row[SOURCE_AIRPORT], row[DESTINATION_AIRPORT])
+        print(row[DEMAND], row[DISTANCE_KM])
+        print(row[DISTANCE_KM]*KMtoM/Boeing_737_600*gas)
 
 
-
-#extracting distances from distance matrix.csv
-for i in range(31):
-    for j in range(i+1, 31):
-        #grab distance from csv
