8d31d7a22a7e14626d9c6ea444fcb6eca536ba5a
PBlue2001 <148484584+PBlue2001@users.noreply.github.com>
2024-02-05 18:00:41 -0500

    added profit per hour script and csv

--- scripts/flight_profit_per_hour.py
+++ scripts/flight_profit_per_hour.py
@@ -13,7 +13,7 @@ DESTINATION_AIRPORT = 1
 DISTANCE_KM = 2
 DEMAND = 3
 
-# linear_aircraft_specs.csv
+# aircraft_specs.csv
 # aircraft,passenger_capacity,cruise_speed (km/h),max_fuel_capacity (gallons),max_range (km),mpg (mpg)
 AIRCRAFT = 0
 CAPACITY = 1
@@ -22,6 +22,14 @@ MAX_FUEL = 3
 MAX_RANGE = 4
 MPG = 5
 
+# fuel_capacity.csv
+SOURCE_AIRPORT = 0
+DESTINATION_AIRPORT = 1
+FUEL_737_600 = 2
+FUEL_737_800 = 3
+FUEL_A200 = 4
+FUEL_A220 = 5
+
 
 # Formula constants
 takeoff_fee = 2000
@@ -29,19 +37,20 @@ landing_fee = 2000
 gas = 6.19              # gallons
 KMtoM = 0.621371        # converts to miles
 break_even_percentage = 0.3
+#cruise_speed_factor = .9
 
 # File-reading constants
 FILE_START = 0
 
 
-with open ("data/flights.csv", "r") as flight_data, open ("data/aircraft_specs.csv", "r") as aircraft_spec_data, open("data/flight_profit_or_loss_profit.csv", "w") as outfile1, open("data/flight_profit_or_loss_loss.csv", "w") as outfile2:
+with open ("data/flights.csv", "r") as flight_data, open ("data/aircraft_specs.csv", "r") as aircraft_spec_data,open ("data/flight_fuel_capacity.csv") as fuel_data, open("data/flight_profit_per_hour.csv", "w") as outfile:
     #Generate break even costs
     flight_reader = csv.reader(flight_data, delimiter=',')
     aircraft_spec_reader = csv.reader(aircraft_spec_data, delimiter=',')
+    fuel_reader = csv.reader(fuel_data, delimiter=',')
     
     #write header of outfile outside loop
-    outfile1.write("source airport,destination airport,airplane type,total Cost,break even cost per ticket, profit per flight, percent full\n")
-    outfile2.write("source airport,destination airport,airplane type,total Cost,break even cost per ticket, loss per flight, percent full\n")
+    outfile.write("source airport,destination airport,airplane type,flight_hours,total Cost,break even cost per ticket, profit/loss per flight,profit/loss per hour, percent full\n")
     #skip header row
     _ = next(flight_reader)
     _ = next(aircraft_spec_reader)
@@ -58,10 +67,9 @@ with open ("data/flights.csv", "r") as flight_data, open ("data/aircraft_specs.c
         for row_aircraft_spec in aircraft_spec_reader: 
             aircraft_name = row_aircraft_spec[AIRCRAFT]
             capacity = int(row_aircraft_spec[CAPACITY])
+            cruise_speed = int(row_aircraft_spec[SPEED])
             mpg = float(row_aircraft_spec[MPG])
 
-
-            #clac
             # Calculate total cost
             total_cost = (distance_km * KMtoM / mpg ) * gas + takeoff_fee + landing_fee
             
@@ -72,13 +80,18 @@ with open ("data/flights.csv", "r") as flight_data, open ("data/aircraft_specs.c
             profit_loss = break_even_cost_per_ticket * demand - total_cost
             #running_total +=profit_loss
 
+            #calc percent full
             percent_full = demand/capacity
+            
+            #calc flight hours
+            flight_hours = distance_km/cruise_speed
+            
+            #calc profit/hour
+            profit_per_hour = profit_loss/flight_hours
 
             #4. Write body
-            if profit_loss > 0:
-                outfile1.write(f"{source_airport},{destination_airport},{aircraft_name},{total_cost},{break_even_cost_per_ticket},{profit_loss},{percent_full}\n")
-            else:
-                outfile2.write(f"{source_airport},{destination_airport},{aircraft_name},{total_cost},{break_even_cost_per_ticket},{profit_loss},{percent_full}\n")
+            if profit_loss>0:
+                outfile.write(f"{source_airport},{destination_airport},{aircraft_name},{flight_hours},{total_cost},{break_even_cost_per_ticket},{profit_loss},{profit_per_hour},{percent_full}\n")
             
             # Print information
             #print(f"Source: {source_airport}, Destination: {destination_airport}")
