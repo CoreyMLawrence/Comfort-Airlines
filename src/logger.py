2a3e68a1a0d97cd8ca060eebd55432d15cc6ac3b
Ethan Cox <ethanrcox@gmail.com>
2024-02-17 13:10:32 -0500

    added simulation time hack

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,5 @@
-#from inspect import currentframe, getframeinfo
 import inspect
+from reference_wrapper import ReferenceWrapper
 
 def processor_code_location(logger, log_method, event_dict):
     STRUCTLOG_STACK_FRAME_OFFSET = 4
@@ -15,7 +15,15 @@ class ProcessorID:
     def __init__(self, id_seed: int = 0):
         self.id = id_seed
 
-    def __call__(self, _, __, event_dict) -> str:
+    def __call__(self, _, __, event_dict):
         event_dict["id"] = self.id
         self.id += 1
+        return event_dict
+    
+class ProcessorSimulationTime:
+    def __init__(self, wrapper: ReferenceWrapper):
+        self.wrapper = wrapper
+
+    def __call__(self, _, __, event_dict): 
+        event_dict["simulation_time"] = self.wrapper.value
         return event_dict
--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ import structlog
 import logger
 import time
 
+from reference_wrapper import ReferenceWrapper
+
 def main() -> None:
     """The entry point for the application"""
+    simulation_time = ReferenceWrapper(0)
+    
     structlog.configure(
         processors=[
             logger.processor_code_location,
             logger.ProcessorID(0),
+            logger.ProcessorSimulationTime(simulation_time),
             structlog.contextvars.merge_contextvars,
             structlog.processors.add_log_level,
             structlog.processors.TimeStamper(fmt="%H:%M:%S", utc=True, key="real_time"),
@@ -19,6 +24,7 @@ def main() -> None:
 
     while True:
         log.info("Hello, comfort airlines!")
+        simulation_time.value += 1
         time.sleep(5)
 
 if __name__ == "__main__":
