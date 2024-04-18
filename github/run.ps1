ccb21757f1183f6cfdf540357cc39ed7d6a2de26
Anthony Cox <ethanrcox@gmail.com>
2024-04-18 16:58:58 -0400

    update run.ps1

--- github/run.ps1
+++ github/run.ps1
@@ -3,13 +3,17 @@ $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $PIPELINE = "./scripts/pipeline.ps1"
 $DEPENDENCIES = "requirements.txt"
 $SIMULATION = "./src/main.py"
+$GUI = "../webdriver.py"
+$STEPS = 4
 
-Write-Host "[INFO: 1/3] Executing script pipeline: generating static data from ./data"
+Write-Host "[INFO: 1/$STEPS] Executing script pipeline: generating static data from ./data"
 powershell $PIPELINE
 
-Write-Host "[INFO: 2/3] Installing dependecies using pip3"
+Write-Host "[INFO: 2/$STEPS] Installing dependecies using pip3"
 pip3 install -r $DEPENDENCIES
 
-Write-Host -NoNewline "[INFO: 3/3] Running simulation!"
+Write-Host "[INFO: 3/$STEPS] Running simulation!"
 python3 $SIMULATION
 
+Write-Host "[INFO: 4/$STEPS] Running GUI"
+python3 $GUI
