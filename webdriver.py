9b7e1bca181ac2fefb2ef3d3039fd974d3eee04f
Corey Lawrence <121775280+CoreyMLawrence@users.noreply.github.com>
2024-05-18 20:03:11 -0400

    Changed gradients back to original design, and removed requirement for the software to be run on a webserver.

--- webdriver.py
+++ webdriver.py
@@ -1,9 +1,20 @@
 import webview
-input("Press enter to run user interface")
+import threading
+import subprocess
+import os
+
+def start_server():
+    # Bind to a specific loopback address to avoid port overlap
+    subprocess.Popen(["python3", "-m", "http.server", "-b", "127.0.0.2"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
 if __name__ == '__main__':
+    # Start the HTTP server in a separate thread
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+    
     # Create a browser window with the specified URL
-    webview.create_window("Comfort Airlines", "http://127.0.0.1/comfort-airlines", fullscreen=True)
+    webview.create_window("Comfort Airlines", "http://127.0.0.2:8000", fullscreen=True)
 
     # Run the GUI event loop
     webview.start()
-    
